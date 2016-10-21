c76151c735d4a3a751a2b5aaa88b9fbfc8174602
badi ifaoui <badi1wow@gmail.com>
2016-10-21 12:05:31 +0100

    removed old cod

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    api_url: '/api/',  // The Api link should end with `/` => 'http://api.example.com/'
-    JWT_TOKEN: 'token',                 // default api token name in user local storage 'token',
-};
